202ea328f8004449e3fd7c87a46228deedb58c7f
Daniel <dc36205@gmail.com>
2024-01-05 14:52:18 -0800

    Added modification to the coding file

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import time
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
@@ -14,7 +19,9 @@ if hist_button: # al hacer clic en el botón
         
             # mostrar un gráfico Plotly interactivo
             st.plotly_chart(fig, use_container_width=True) 
+            time.sleep(0.05)
 
+progress_bar.empty()
 
 #import streamlit as st
 
@@ -28,4 +35,5 @@ if build_histogram: # si la casilla de verificación está seleccionada
         
             # mostrar un gráfico Plotly interactivo
             st.plotly_chart(fig, use_container_width=True) 
+
 
