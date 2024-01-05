367f48d4f2a83b1379b67ad12e4936ffb414eada
Daniel <dc36205@gmail.com>
2024-01-05 14:06:20 -0800

    Added modification to the coding file

---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+        
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+            fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True) 
+
+
+#import streamlit as st
+
+# crear una casilla de verificación para el scatter
+build_histogram = st.checkbox('Construir un scatter')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+            st.write('Construir un scatter para la columna odómetro')
+            # crear un scatter
+            fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True) 
+
